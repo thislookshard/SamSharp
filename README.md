e9ce8452af907f4be1fba08e87cbcba9663ac6d8
Sam Luedtke <luedtkesj@gmail.com>
2023-12-17 21:19:13 -0600

    Added encoder export script

--- README.md
+++ README.md
@@ -5,9 +5,10 @@ A C# interface for Meta's Segment Anything Model (SAM). This repo contains a sma
 The ImageUtility class uses System.Drawing for image processing; therefore this class is currently only compatable with Windows. Eventually I will migrate this to a cross-platform library if this project gets a full nuget release.
 
 ### Generating the Models ###
+
 There are two models needed for this to run
 
-1. <b>Encoder:</b> The encoder needs to be exported from the <i>export_encoder.py</i> script. The PyTorch model will need to be downloaded from either the [Segment Anything repo's](https://github.com/facebookresearch/segment-anything) checkpoints or from [MobileSAM's](https://github.com/ChaoningZhang/MobileSAM/blob/master/weights/mobile_sam.pt) repo for a faster and much lighter model (recommended if your harder isn't as beefy)
+1. <b>Encoder:</b> The encoder needs to be exported from the <i>export_encoder.py</i> script. The PyTorch model will need to be downloaded from either the [Segment Anything repo's](https://github.com/facebookresearch/segment-anything) checkpoints or from [MobileSAM's](https://github.com/ChaoningZhang/MobileSAM/blob/master/weights/mobile_sam.pt) repo for a faster and much lighter model (recommended if your hardware isn't as beefy)
 2. <b>Decoder:</b> The decoder model can be generated from the Segment Anything's [Onnx Model Example's](https://github.com/facebookresearch/segment-anything/blob/main/notebooks/onnx_model_example.ipynb) notebook
 
-Once these are generated, chose these as the encoder and decoder model files when prompted and watch the magic happen.
+Once these are generated, choose these as the encoder and decoder model files when prompted and watch the magic happen.
---
+++ export_encoder.py
@@ -0,0 +1,61 @@
+import torch
+import numpy as np
+import os
+from mobile_sam import sam_model_registry, SamPredictor
+#from segment_anything import sam_model_registry, SamPredictor
+from segment_anything.utils.transforms import ResizeLongestSide
+from onnxruntime.quantization import QuantType
+from onnxruntime.quantization.quantize import quantize_dynamic
+
+# Generate preprocessing model of Segment-anything in onnx format
+# Target image size is 1024x720
+image_size = (1024, 1024)
+# Download Segment-anything model "sam_vit_h_4b8939.pth" from https://github.com/facebookresearch/segment-anything#model-checkpoints
+# and change the path below
+checkpoint = 'mobile_sam.pt'
+model_type = "vit_t"
+output_path = 'mobile_encoder.onnx'
+quantize = False
+
+
+# The raw directory can be deleted after the quantization is done
+output_raw_path = os.path.dirname(output_path) + '/raw/sam_preprocess.onnx'
+os.makedirs(os.path.dirname(output_raw_path), exist_ok=True)
+
+sam = sam_model_registry[model_type](checkpoint=checkpoint)
+sam.to(device='cpu')
+transform = ResizeLongestSide(sam.image_encoder.img_size)
+
+image = np.zeros((image_size[1], image_size[0], 3), dtype=np.uint8)
+input_image = transform.apply_image(image)
+input_image_torch = torch.as_tensor(input_image, device='cpu')
+input_image_torch = input_image_torch.permute(
+    2, 0, 1).contiguous()[None, :, :, :]
+
+
+class Model(torch.nn.Module):
+    def __init__(self, image_size, checkpoint, model_type):
+        super().__init__()
+        self.sam = sam_model_registry[model_type](checkpoint=checkpoint)
+        self.sam.to(device='cpu')
+        self.predictor = SamPredictor(self.sam)
+        self.image_size = image_size
+
+    def forward(self, x):
+        self.predictor.set_torch_image(x, (self.image_size))
+        return self.predictor.get_image_embedding()
+
+
+model = Model(image_size, checkpoint, model_type)
+model_trace = torch.jit.trace(model, input_image_torch)
+torch.onnx.export(model_trace, input_image_torch, output_raw_path,
+                  input_names=['input'], output_names=['output'])
+
+quantize_dynamic(
+    model_input=output_raw_path,
+    model_output=output_path,
+    optimize_model=True,
+    per_channel=False,
+    reduce_range=False,
+    weight_type=QuantType.QUInt8,
+)
